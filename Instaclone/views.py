79338f85a3998f8f600cb0de2a3669832b4cafdd
MukeshDubey1420 <mukeshdubey1420@gmail.com>
2018-07-08 00:19:08 +0530

    more models like ,comment,are added

--- Instaclone/views.py
+++ Instaclone/views.py
@@ -27,13 +27,13 @@
 
 
 from django.shortcuts import render, redirect
-from .forms import SignUpForm, LoginForm, PostForm
-from .models import UserModel, SessionToken, PostModel
+from .forms import SignUpForm, LoginForm, PostForm, LikeForm, CommentForm
+from .models import UserModel, SessionToken, PostModel, LikeModel, CommentModel
 from django.contrib.auth.hashers import make_password, check_password
 from My_Django_Project.settings import BASE_DIR
 from datetime import timedelta
 from django.utils import timezone
-
+import os
 from imgurpython import ImgurClient
 # Create your views here.
 
@@ -106,9 +106,7 @@ def post_view(request):
                 caption = form.cleaned_data.get('caption')
                 post = PostModel(user=user, image=image, caption=caption)
                 post.save()
-
-                path = str(BASE_DIR + post.image.url)
-
+                path = os.path.join(BASE_DIR, post.image.url)
                 client = ImgurClient(client_id, client_secret)
                 post.image_url = client.upload_from_path(path,anon=True)['link']
                 post.save()
@@ -122,6 +120,17 @@ def post_view(request):
         return redirect('/login/')
 
 
+
+#For validating the session
+def check_validation(request):
+    if request.COOKIES.get('session_token'):
+        session = SessionToken.objects.filter(session_token=request.COOKIES.get('session_token')).first()
+        if session:
+            time_to_live = session.created_on + timedelta(days=1)
+            if time_to_live > timezone.now():
+                return session.user
+    else:
+        return None
 
 
 
@@ -137,16 +146,38 @@ def feed_view(request):
         return redirect('/login/')
 
 
+def like_view(request):
+    user = check_validation(request)
+    if user and request.method == 'POST':
+        form = LikeForm(request.POST)
+        if form.is_valid():
+            post_id = form.cleaned_data.get('post').id
 
+            existing_like = LikeModel.objects.filter(post_id=post_id, user=user).first()
 
+            if not existing_like:
+                LikeModel.objects.create(post_id=post_id, user=user)
+            else:
+                existing_like.delete()
 
-#For validating the session
-def check_validation(request):
-    if request.COOKIES.get('session_token'):
-        session = SessionToken.objects.filter(session_token=request.COOKIES.get('session_token')).first()
-        if session:
-            time_to_live = session.created_on + timedelta(days=1)
-            if time_to_live > timezone.now():
-                return session.user
+            return redirect('/feed/')
+
     else:
-        return None
+        return redirect('/login/')
+
+
+
+def comment_view(request):
+    user = check_validation(request)
+    if user and request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            post_id = form.cleaned_data.get('post').id
+            comment_text = form.cleaned_data.get('comment_text')
+            comment = CommentModel.objects.create(user=user, post_id=post_id, comment_text=comment_text)
+            comment.save()
+            return redirect('/feed/')
+        else:
+            return redirect('/feed/')
+    else:
+        return redirect('/login')
--- My_Django_Project/urls.py
+++ My_Django_Project/urls.py
@@ -32,13 +32,15 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from Instaclone.views import signup_view,login_view,feed_view,post_view
+from Instaclone.views import signup_view,login_view,feed_view,post_view,like_view, comment_view
 urlpatterns = [
     url(r'^jet/', include('jet.urls', 'jet')),       # Django JET URLS
     url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
     url(r'^admin/', admin.site.urls),
     url('post/', post_view),
     url('feed/', feed_view),
+    url('like/', like_view),
+    url('comment/', comment_view),
     url('login/', login_view),
     url('', signup_view),
 
